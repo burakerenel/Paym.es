9e3d1e6614f353a36073dfa41320ffc3b61bfa27
Burak Erenel <burakerenel@gmail.com>
2019-09-17 12:18:33 +0300

    Update createOrder.php

--- createOrder.php
+++ createOrder.php
@@ -3,25 +3,23 @@
 
     $url = 'https://api.paym.es/order_create';
     $data_array = array(
-    	'secret'=>'{SecretKey}', // teknik@paym.es tarafından ulaştırılan secret keyiniz.
-    	'orderId'=>'100',	// Kendi sisteminizdeki ödeme işlem numarasıdır.
-    	'price'=>'10.00',
-    	'currency'=>'TRY',
-    	'productName'=>'Mavi Kalem',
+        'secret'=>'{SecretKey}', // teknik@paym.es tarafından ulaştırılan secret keyiniz.
+        'orderId'=>'100',   // Kendi sisteminizdeki ödeme işlem numarasıdır.
+        'price'=>'10.10',
+        'currency'=>'TRY',
+        'productName'=>'Mavi Kalem',
         'buyerName'=>'Burak Erenel',
         'buyerPhone'=>'905555555555',
         'buyerEmail' => 'info@burakerenel.com',
         'buyerAddress'=>'Kadıköy'
     );
-    $data = json_encode($data_array);
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $veri);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data_array));
     $curl_exec = curl_exec ($ch);
     curl_close ($ch);
     $result = json_decode($curl_exec);
-
     if($result->status == 'error')
         echo $result->message;
     elseif($result->status == 'return_url'){
